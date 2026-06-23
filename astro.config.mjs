f1a6bbfbfe46f7e20c590ca099c4baf648992412
Siriuns-X <Siriuns@icloud.com>
2026-06-23 17:30:32 +0800

    fix(config): set correct GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://Source-of-USTB.github.io",
-    base: "/planet/",
+    base: "/Planet-Source-of-USTB/",
 });
